37c30873b0f4c88f609d9dabb809a378c891720f
zzswang <zzswang@gmail.com>
2017-03-31 17:13:46 +0800

    add wali

---
+++ wali.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class Duotai < Formula
+  homepage 'https://zzswang.github.io'
+  url 'https://github.com/zzswang/wali/releases/download/v0.1.0/wali'
+
+  def install
+    bin.install 'wali'
+  end
+
+  # test do
+  #   system bin/'pdftk', '--version'
+  # end
+end
